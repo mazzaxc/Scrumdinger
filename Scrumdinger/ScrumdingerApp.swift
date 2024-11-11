587c4538e7b3549847754a3eba82d2367527ebe5
Lorenzo Mazzarotto <mazzaxc@icloud.com>
2024-11-11 07:57:05 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Lorenzo Mazzarotto on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
